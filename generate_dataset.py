1bad55f9ad673faea2a4fc2326cec9fadf167645
JosselinSomervilleRoberts <josselin.somerville@polytechnique.edu>
2023-05-04 00:31:25 -0700

    Cleanup generate_dataset

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,53 +1,134 @@
-import labelbox
 import requests
 from PIL import Image
 from io import BytesIO
 from tqdm import tqdm
-
-# Get the labelbox project
-LB_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjbGg0NHRyODEwNnFpMDcxMWU1dWdjM2Q3Iiwib3JnYW5pemF0aW9uSWQiOiJjbGg0NHRyN3AwNnFoMDcxMWNtbGM1Z29lIiwiYXBpS2V5SWQiOiJjbGg4MmhzYmIwY2N1MDcwamM0eHY4Z29iIiwic2VjcmV0IjoiNTM2MDI0NDFmMDY4OGM2ZTIyZmJhMjFjZWQxYzk2MWQiLCJpYXQiOjE2ODMxNDA2NjQsImV4cCI6MjMxNDI5MjY2NH0.AfCKYZ-Bc7xA5i3ybMJIaLIHB8NB7_NXt41vcZH7Z-8"
-lb = labelbox.Client(api_key=LB_API_KEY)
-project = lb.get_project('clh44xst90kru07zfdq3qdf72')
-
-print("Downloading labels...")
-labels = project.label_generator()
-labels = project.export_labels(download = True)
-print("Done")
-
-# Create folder name "dataset" if it doesn't exist
+import configparser
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 import os
-if not os.path.exists("dataset"):
-    os.makedirs("dataset")
+from typing import List
 
-# Download all images and save them in the "dataset" folder
-print("Downloading images...")
-for i in tqdm(range(len(labels))):
-    x = labels[i]['Labeled Data']
-    response = requests.get(x)
-    img = Image.open(BytesIO(response.content))
+# config
+config = configparser.ConfigParser()
+config.read('config.ini')
+DATASET_FOLDER = config["PATHS"]["LABELBOX_DATASET"]
+LB_API_KEY = config['SECRETS']['LABELBOX_API_KEY']
+LB_PROJECT_ID = config['SECRETS']['LABELBOX_PROJECT_ID']
+CLASS_TO_COLOR = {
+        "edge": [int(c) for c in config["DISPLAY"]["COLOR_EDGE"].split(' ')],
+        "crack": [int(c) for c in config["DISPLAY"]["COLOR_CRACK"].split(' ')],
+        "boulder": [int(c) for c in config["DISPLAY"]["COLOR_BOULDER"].split(' ')],
+        "rough_patch": [int(c) for c in config["DISPLAY"]["COLOR_ROUGH_PATCH"].split(' ')],
+    }
 
-    # Create folder named i if it doesn't exist
-    if not os.path.exists("dataset/" + str(i)):
-        os.makedirs("dataset/" + str(i))
-    else:
-        print(f"Image {i} already exists")
-        continue
+def check_if_dataset_exists() -> bool:
+    """Checks if the dataset already exists."""
+    return os.path.exists(DATASET_FOLDER)
 
-    # Save image
-    img.save("dataset/" + str(i) + "/image.png")
+def get_masks(image_number: int) -> list:
+    """Gets the masks for a given image number."""
+    masks = []
+    img_folder = os.path.join(DATASET_FOLDER, str(image_number))
+    for mask_path in os.listdir(img_folder):
+        if mask_path.endswith(".png") and mask_path.startswith("mask"):
+            mask = Image.open(os.path.join(img_folder, mask_path))
+            masks.append(mask)
+    return masks
 
-    # Get all masks
+def get_classes(image_number: int) -> List[str]:
+    """Gets the classes for a given image number."""
     classes = []
-    objects = labels[i]['Label']['objects']
-    for j in range(len(objects)):
-        x = objects[j]['instanceURI']
+    img_folder = os.path.join(DATASET_FOLDER, str(image_number))
+    with open(os.path.join(img_folder, "classes.txt"), "r") as f:
+        classes = f.read().split("\n")
+    return classes
+
+def generate_labelbox_dataset() -> None:
+    import labelbox
+
+    # Get the labelbox project
+    lb = labelbox.Client(api_key=LB_API_KEY)
+    project = lb.get_project(LB_PROJECT_ID)
+
+    print("Downloading labels...")
+    labels = project.label_generator()
+    labels = project.export_labels(download = True)
+    print("Done")
+
+    # Create folder name "dataset" if it doesn't exist
+    import os
+    if not os.path.exists(DATASET_FOLDER):
+        os.makedirs(DATASET_FOLDER)
+
+    # Download all images and save them in the "dataset" folder
+    print("Downloading images...")
+    for i in tqdm(range(len(labels))):
+        x = labels[i]['Labeled Data']
         response = requests.get(x)
         img = Image.open(BytesIO(response.content))
-        img.save("dataset/" + str(i) + "/mask_" + str(j) + ".png")
-        classes.append(objects[j]['title'])
 
-    # Save classes
-    with open("dataset/" + str(i) + "/classes.txt", "w") as f:
-        f.write("\n".join(classes))
+        # Create folder named i if it doesn't exist
+        img_folder = os.path.join(DATASET_FOLDER, str(i))
+        if not os.path.exists(img_folder):
+            os.makedirs(img_folder)
+        else:
+            print(f"Image {i} already exists")
+            continue
 
-print("Done")
+        # Save image
+        img.save(img_folder + "/image.png")
+
+        # Get all masks
+        classes = []
+        objects = labels[i]['Label']['objects']
+        for j, object in enumerate(objects):
+            instanceURI = object['instanceURI']
+            response = requests.get(instanceURI)
+            img = Image.open(BytesIO(response.content))
+            img = Image.eval(img, lambda a: 255 - a)
+            img.putalpha(255)
+            img.save(img_folder + "/mask_" + str(j) + ".png")
+            classes.append(objects[j]['title'])
+
+        # Save classes
+        with open(img_folder + "/classes.txt", "w") as f:
+            f.write("\n".join(classes))
+    print("Done")
+
+def show_img_with_all_masks(image_number: int) -> None:
+    """Shows the image with all the masks on top of it."""
+    # Load the image
+    img_folder = os.path.join(DATASET_FOLDER, str(image_number))
+    img = cv2.imread(img_folder + "/image.png")
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Load the masks
+    masks = get_masks(image_number)
+    classes = get_classes(image_number)
+
+    for i, mask in enumerate(masks):
+        # Draw the mask on top of the image as class_to_color[classes[i]] with an alpha of 0.5 where
+        # the mask is not 0.
+        mask_img = np.array(mask)[:,:,0]
+        color = CLASS_TO_COLOR[classes[i]]
+        color.append(0.5)
+        
+        # Apply the color where mask_img is 0 with alpha blending
+        img[mask_img == 0] = img[mask_img == 0] * (1 - color[3]) + np.array(color[:3]) * color[3]
+        
+
+    # Show the image with all the masks on top of it
+    plt.figure(figsize=(10,10))
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
+
+
+if __name__ == "__main__":
+    if not check_if_dataset_exists():
+        print("Dataset does not exist")
+        generate_labelbox_dataset()
+    else:
+        print("Dataset already exists")
+        show_img_with_all_masks(0)
--- generate_finetuning_dataset.py
+++ generate_finetuning_dataset.py
@@ -2,15 +2,70 @@ import os
 from tqdm import tqdm
 from PIL import Image
 from PIL import ImageDraw
+from typing import List, Dict
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import configparser
+from generate_dataset import check_if_dataset_exists
+
+# config
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 CLASSES = ["edge", "boulder", "crack"]
-DATASET_FOLDER = "./dataset"
-OUTPUT_FOLDER = "./finetuning_dataset"
+DATASET_FOLDER = config["PATHS"]["LABELBOX_DATASET"]
+OUTPUT_FOLDER = config["PATHS"]["FINETUNE_DATASET"]
 
-def mask_to_bbox(mask):
+def load_gt_masks() -> dict: # -> Dict[int, Image]:
+    """Loads the ground truth masks from the OUTPUT_FOLDER folder."""
+    ground_truth_masks = {}
+    for k in range(len(os.listdir(OUTPUT_FOLDER))):
+        mask_path = os.path.join(OUTPUT_FOLDER, "masks", str(k) + ".png")
+        gt_grayscale = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+        ground_truth_masks[int(k)] = (gt_grayscale == 0)
+    return ground_truth_masks
+
+def load_bbox_coords() -> List[np.ndarray]:
+    """Loads the bounding box coordinates from the OUTPUT_FOLDER folder."""
+    bbox_coords = {}
+    for k in range(len(os.listdir(OUTPUT_FOLDER))):
+        bbox_path = os.path.join(OUTPUT_FOLDER, "bboxes", str(k) + ".txt")
+        with open(bbox_path, "r") as f:
+            bbox = np.array([int(x) for x in f.read().split()])
+        bbox_coords[int(k)] = bbox
+    return bbox_coords
+
+# Helper function provided in https://github.com/facebookresearch/segment-anything/blob/9e8f1309c94f1128a6e5c047a10fdcb02fc8d651/notebooks/predictor_example.ipynb
+def show_mask(mask: Image, ax: plt.Axes, color: tuple = None) -> None:
+    if color is None:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+# Helper function provided in https://github.com/facebookresearch/segment-anything/blob/9e8f1309c94f1128a6e5c047a10fdcb02fc8d651/notebooks/predictor_example.ipynb
+def show_box(box: np.ndarray, ax: plt.Axes) -> None:
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))
+
+def show_image_with_mask(image_number: int, bbox_coords: List[np.ndarray], ground_truth_masks: dict) -> None:
+    image = cv2.imread(f"{OUTPUT_FOLDER}/images/{image_number}.png")
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    plt.figure(figsize=(10,10))
+    plt.imshow(image)
+    show_box(bbox_coords[image_number], plt.gca())
+    show_mask(ground_truth_masks[image_number], plt.gca())
+    plt.axis('off')
+    plt.show()
+
+def mask_to_bbox(mask: Image) -> np.ndarray:
+    """Converts a mask to a bounding box such that the bounding box contains the mask.
+    The bounding box is generated with some noise and padding."""
     # Convert the mask to a binary mask
     mask = mask.convert("L")
-    mask = mask.point(lambda x: 0 if x < 128 else 255, '1')
+    mask = mask.point(lambda x: 0 if x >= 128 else 255, '1')
     mask = mask.convert("1")
 
     # Get the bounding box
@@ -19,95 +74,111 @@ def mask_to_bbox(mask):
     # Add some noise and padding to the bounding box
     bbox = (bbox[0] - 10, bbox[1] - 10, bbox[2] + 10, bbox[3] + 10)
 
-    # Convert the bounding box to a binary mask
-    bbox_mask = Image.new("1", mask.size)
+    # Crop the bounding box so that it is within the image
+    bbox = (max(bbox[0], 0), max(bbox[1], 0), min(bbox[2], mask.width), min(bbox[3], mask.height))
 
-    # Draw the bounding box
-    bbox_mask_draw = ImageDraw.Draw(bbox_mask)
-    bbox_mask_draw.rectangle(bbox, fill=1)
+    # Convert the bounding box to a numpy array
+    bbox = np.array(bbox)
 
-    return bbox_mask
+    return bbox
 
-# Check if the dataset folder exists
-if not os.path.exists(DATASET_FOLDER):
-    raise Exception("Dataset folder not found. Please run generate_dataset.py first.")
 
-# Get the number of images in the dataset
-num_images = len(os.listdir(DATASET_FOLDER))
-print(f"Found {num_images} images")
+def check_if_finetuning_dataset_exists() -> bool:
+    """Checks if the finetuning dataset exists."""
+    return os.path.exists("finetuning_dataset")
 
-# Create the output folder
-if not os.path.exists("finetuning_dataset"):
-    os.makedirs("finetuning_dataset")
-if not os.path.exists("finetuning_dataset/images"):
-    os.makedirs("finetuning_dataset/images")
-if not os.path.exists("finetuning_dataset/masks"):
-    os.makedirs("finetuning_dataset/masks")
-if not os.path.exists("finetuning_dataset/bboxes"):
-    os.makedirs("finetuning_dataset/bboxes")
 
-# For each image folder, open classes.txt and get the classes
-# For each line (i.e. mask) in classes.txt, load the mask if the class is in CLASSES
-# Then generate a bounding box for the mask and save it in the output folder
-# The bounding box is saved as a binary mask
-# It if generated with some noise and padding
+def generate_finetuning_dataset() -> None:
+    """Generates the finetuning dataset from the dataset folder."""
 
-# The final architecture of the dataset folder is:
-# finetuning_dataset
-# ├── images
-# │   ├── 0.png
-# │   ├── 1.png
-# │   ├── ...
-# │   └── N.png
-# │── masks
-# │   ├── 0.png
-# │   ├── 1.png
-# │   ├── ...
-# │   └── N.png
-# │── bboxes
-# │   ├── 0.png
-# │   ├── 1.png
-# │   ├── ...
-# │   └── N.png
+    # Check if the dataset folder exists
+    if not check_if_dataset_exists():
+        raise Exception("Dataset folder not found. Please run generate_dataset.py first.")
 
-num_masks_added = 0
-num_masks_failed = 0
+    # Get the number of images in the dataset
+    num_images = len(os.listdir(DATASET_FOLDER))
+    print(f"Found {num_images} images")
 
-for i in tqdm(range(num_images)):
-    img_folder = os.path.join(DATASET_FOLDER, str(i))
-    classes_file = os.path.join(img_folder, "classes.txt")
+    # Create the output folder
+    if not os.path.exists(OUTPUT_FOLDER):
+        os.mkdir(OUTPUT_FOLDER)
+        os.mkdir(os.path.join(OUTPUT_FOLDER, "images"))
+        os.mkdir(os.path.join(OUTPUT_FOLDER, "masks"))
+        os.mkdir(os.path.join(OUTPUT_FOLDER, "bboxes"))
 
-    # Get the classes
-    with open(classes_file, "r") as f:
-        classes = f.read().splitlines()
+    # For each image folder, open classes.txt and get the classes
+    # For each line (i.e. mask) in classes.txt, load the mask if the class is in CLASSES
+    # Then generate a bounding box for the mask and save it in the output folder
+    # The bounding box is saved as a binary mask
+    # It if generated with some noise and padding
 
-    # Get the masks
-    for j, class_mask in enumerate(classes):
-        if class_mask in CLASSES:
-            try:
-                # Load the mask
-                mask_path = os.path.join(img_folder, "mask_" + str(j) + ".png")
-                mask = Image.open(mask_path)
+    # The final architecture of the dataset folder is:
+    # <OUTPUT_FOLDER>
+    # ├── images
+    # │   ├── 0.png
+    # │   ├── 1.png
+    # │   ├── ...
+    # │   └── N.png
+    # │── masks
+    # │   ├── 0.png
+    # │   ├── 1.png
+    # │   ├── ...
+    # │   └── N.png
+    # │── bboxes
+    # │   ├── 0.png
+    # │   ├── 1.png
+    # │   ├── ...
+    # │   └── N.png
 
-                # Generate the bounding box
-                bbox = mask_to_bbox(mask)
+    num_masks_added = 0
+    num_masks_failed = 0
 
-                # Save the mask
-                mask.save(os.path.join(OUTPUT_FOLDER, "masks", str(num_masks_added) + ".png"))
+    for i in tqdm(range(num_images)):
+        img_folder = os.path.join(DATASET_FOLDER, str(i))
+        classes_file = os.path.join(img_folder, "classes.txt")
 
-                # Copy the image
-                img_path = os.path.join(img_folder, "image.png")
-                img = Image.open(img_path)
-                img.save(os.path.join(OUTPUT_FOLDER, "images", str(num_masks_added) + ".png"))
+        # Get the classes
+        with open(classes_file, "r") as f:
+            classes = f.read().splitlines()
 
-                # Save the bbox
-                bbox.save(os.path.join(OUTPUT_FOLDER, "bboxes", str(num_masks_added) + ".png"))
+        # Get the masks
+        for j, class_mask in enumerate(classes):
+            if class_mask in CLASSES:
+                try:
+                    # Load the mask
+                    mask_path: str = os.path.join(img_folder, "mask_" + str(j) + ".png")
+                    mask: Image = Image.open(mask_path)
 
-                num_masks_added += 1
-            except Exception as e:
-                num_masks_failed += 1
-                print(f"Error processing image {i} mask {j}: {e}")
-                continue
+                    # Generate the bounding box
+                    bbox: np.ndarray = mask_to_bbox(mask)
 
-print(f"Generated {num_masks_added} masks")
-print(f"Failed to generate {num_masks_failed} masks")
+                    # Save the mask
+                    mask.save(os.path.join(OUTPUT_FOLDER, "masks", str(num_masks_added) + ".png"))
+
+                    # Copy the image
+                    img_path: str = os.path.join(img_folder, "image.png")
+                    img: Image = Image.open(img_path)
+                    img.save(os.path.join(OUTPUT_FOLDER, "images", str(num_masks_added) + ".png"))
+
+                    # Save the bbox with numpy savetxt
+                    np.savetxt(os.path.join(OUTPUT_FOLDER, "bboxes", str(num_masks_added) + ".txt"), bbox, fmt="%d")
+
+                    num_masks_added += 1
+                except Exception as e:
+                    num_masks_failed += 1
+                    print(f"Error processing image {i} mask {j}: {e}")
+                    continue
+
+    print(f"Generated {num_masks_added} masks")
+    print(f"Failed to generate {num_masks_failed} masks")
+
+
+if __name__ == "__main__":
+    if not check_if_finetuning_dataset_exists():
+        print("Finetuning dataset does not exist. Generating...")
+        generate_finetuning_dataset()
+    else:
+        print("Finetuning dataset already exists. Skipping generation.")
+        gt_masks = load_gt_masks()
+        bbox_coords = load_bbox_coords()
+        show_image_with_mask(0, bbox_coords, gt_masks)
