ef3ac11fc8ec5a26a9900ced6c3f6c53afa743b2
Josselin Somerville <josselin.somerville@gmail.com>
2023-08-06 13:38:16 -0700

    Add control for logging, eval, visualization, saving frequency and created profiles depending on the use case

--- mmsegmentation/config.py
+++ mmsegmentation/config.py
@@ -1,5 +1,9 @@
 # =========== TO CHANGE =========== #
 
+# Name of the run
+# string
+WANDB_RUN_NAME = "Skill"
+
 # Define the model here (2 modifications!) (from custom_configs/models)
 # string
 MODEL = "vit_vit-b16_mln_upernet"
@@ -18,6 +22,10 @@ LOSSES = {
     "cl_dice": 3.0,
 }
 
+# Metric name
+# string
+METRIC = "ReachbotMetric"
+
 # Define the learning rate schedule (from custom_configs/schedules)
 # string
 LR = "aggressive"
@@ -30,9 +38,39 @@ CHECKPOINT = None
 # int
 num_epochs = 100
 
-# Debug mode
-DEBUG_EVERY = 10
+# Debug path
+# string or None
 DEBUG_PATH = "/media/jsomerviller/SSD2/output"
+
+# Logging config, uncomment the one you want
+
+# EXPERIMENTAL (to see results fast)
+# No visualization, no logging, no saving, metrics every few epochs
+LOG_STEP_INTERVAL = -1
+DEBUG_STEP_INTERVAL = -1
+SAVE_EPOCH_INTERVAL = -1
+VISUALIZE_ONE_OUT_OF = -1
+EVAL_EPOCH_INTERVAL = 5
+USE_WANDB = False
+
+# # DEBUGGING (to see what is happening)
+# # Debugging, logging, few visualization, no saving, metrics every few epochs
+# LOG_STEP_INTERVAL = 5
+# DEBUG_STEP_INTERVAL = 10
+# SAVE_EPOCH_INTERVAL = -1
+# VISUALIZE_ONE_OUT_OF = 5
+# EVAL_EPOCH_INTERVAL = 3
+# USE_WANDB = False
+
+# # PRODUCTION (to train a model and save the results)
+# # No debugging but logging, visualization, saving, metrics every epoch
+# LOG_STEP_INTERVAL = 1
+# DEBUG_STEP_INTERVAL = -1
+# SAVE_EPOCH_INTERVAL = 1
+# VISUALIZE_ONE_OUT_OF = 1
+# EVAL_EPOCH_INTERVAL = 1
+# USE_WANDB = True
+
 # ================================= #
 
 
@@ -42,6 +80,7 @@ from custom_configs.memory import *
 
 # Imports
 from custom_configs.main import default_checkpoint, losses_mappings, datasets_mappings, schedules_mappings
+from toolbox.printing import warn
 import importlib
 
 
@@ -73,15 +112,16 @@ del lr_module
 
 
 # Metrics
-val_evaluator = dict(type="ReachbotOldMetric")
+val_evaluator = dict(type=METRIC)
 test_evaluator = val_evaluator
 
 
-# Make sure to evaluate every 1 epoch
+# Make sure to evaluate every EVAL_EPOCH_INTERVAL epochs
+assert EVAL_EPOCH_INTERVAL > 1, "EVAL_EPOCH_INTERVAL must be > 1"
 train_cfg = dict(
     type="IterBasedTrainLoop",
     max_iters=num_epochs * num_training_samples // train_batch_size,
-    val_interval=num_training_samples // train_batch_size
+    val_interval=EVAL_EPOCH_INTERVAL * num_training_samples // train_batch_size
 )
 val_cfg = dict(type="ValLoop")
 test_cfg = dict(type="TestLoop")
@@ -97,7 +137,7 @@ model = dict(
     decode_head=dict(
         num_classes=2,
         loss_decode=[
-            dict(type=losses_mappings[loss_name][0], loss_name=losses_mappings[loss_name][1], loss_weight=LOSSES[loss_name], debug_every=DEBUG_EVERY, debug_path=DEBUG_PATH)
+            dict(type=losses_mappings[loss_name][0], loss_name=losses_mappings[loss_name][1], loss_weight=LOSSES[loss_name], debug_every=DEBUG_STEP_INTERVAL, debug_path=DEBUG_PATH)
             for loss_name in LOSSES
         ],
     ),
@@ -114,26 +154,43 @@ model = dict(
 # Hooks
 default_hooks = dict(
     timer=dict(type="IterTimerHook"),
-    logger=dict(type="LoggerHook", interval=1, log_metric_by_epoch=False),
     param_scheduler=dict(type="ParamSchedulerHook"),
-    checkpoint=dict(type="CheckpointHook", by_epoch=False, interval=num_training_samples // train_batch_size),
     sampler_seed=dict(type="DistSamplerSeedHook"),
-    visualization=dict(type="SegVisualizationHook", draw=True, interval=1),
 )
+if LOG_STEP_INTERVAL > 0:
+    default_hooks["logger"] = dict(type="LoggerHook", interval=LOG_STEP_INTERVAL, log_metric_by_epoch=False)
+else:
+    warn("No logging will be performed.")
+
+if SAVE_EPOCH_INTERVAL > 0:
+    default_hooks["checkpoint"] = dict(type="CheckpointHook", by_epoch=False, interval=SAVE_EPOCH_INTERVAL * num_training_samples // train_batch_size)
+else:
+    warn("No checkpoint will be saved.")
+
+if VISUALIZE_ONE_OUT_OF > 0:
+    default_hooks["visualization"] = dict(type="SegVisualizationHook", draw=True, interval=VISUALIZE_ONE_OUT_OF)
+else:
+    warn("No visualization will be performed.")
+
 
 
 # WANDB logging
 vis_backends = [
     dict(type="LocalVisBackend"),
-    dict(
-        type="WandbVisBackend",
-        init_kwargs=dict(
-            project="cracks_segmentation",
-            entity="single-shot-robot",
-            name="full-skill",
-        ),
-    ),
 ]
+if USE_WANDB:
+    vis_backends.append(
+        dict(
+            type="WandbVisBackend",
+            init_kwargs=dict(
+                project=DATASET + "_segmentation",
+                entity="single-shot-robot",
+                name=WANDB_RUN_NAME,
+            ),
+        ),
+    )
+else:
+    warn("No WANDB logging will be performed.")
 
 
 # Visualization
